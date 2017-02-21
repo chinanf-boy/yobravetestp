95401f6dd5715fb33cbf8202a32ec9163f848baf
chinanf-boy <lizhenyong@yobravedeMacBook-Pro.local>
2017-02-21 22:19:52 +0800

    init

---
+++ spec/yobravetestp-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import YobravetestpView from '../lib/yobravetestp-view';
+
+describe('YobravetestpView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
